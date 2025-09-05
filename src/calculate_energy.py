bb50787e5404451e90815e788b8d40c5d07bd99c
Hossein-shirani <shiranihossein@gmail.com>
2025-09-05 11:00:13 -0700

    Update calculate_energy.py

--- src/calculate_energy.py
+++ src/calculate_energy.py
@@ -4,53 +4,56 @@ from ase import Atoms
 from ase.constraints import FixInternals
 from ase.optimize import BFGS
 
-# Load ANI-1x model
+# Load ANI-1x model with periodic table indexing
 model = torchani.models.ANI1x(periodic_table_index=True)
 
-# Define molecule
-atoms = Atoms(
-    'CCCCCCHHHOHOHCHCHCCCCHCHCHHOH',
-    positions=[
-        [-3.94096600, -1.12879600, 0.20347400],
-        [-2.56061700, -1.32019800, 0.14023800],
-        [-1.71477300, -0.23369100, -0.07406900],
-        [-2.26407500, 1.04431200, -0.24666100],
-        [-3.64111300, 1.21884100, -0.18142200],
-        [-4.49208500, 0.13737100, 0.04679000],
-        [-2.16336500, -2.32231400, 0.27262700],
-        [-1.62043700, 1.89533300, -0.46020600],
-        [-5.56536800, 0.30623400, 0.08666100],
-        [-4.71049000, -2.22922600, 0.42316500],
-        [-5.63815500, -1.96257800, 0.44427100],
-        [-4.23006400, 2.43547800, -0.34562400],
-        [-3.54157200, 3.09456600, -0.49957000],
-        [-0.26026900, -0.47111800, -0.11398200],
-        [0.03062100, -1.50930900, -0.27245000],
-        [0.68875500, 0.45504300, 0.06727900],
-        [0.38867600, 1.47755500, 0.30003300],
-        [2.14257400, 0.22989000, 0.02352400],
-        [3.00345300, 1.18570700, 0.56864300],
-        [2.71996600, -0.91268300, -0.55027700],
-        [4.38222300, 1.00948900, 0.56762500],
-        [2.58529000, 2.08695900, 1.01295500],
-        [4.09085400, -1.10321000, -0.55887900],
-        [2.08396900, -1.66197700, -1.01495300],
-        [4.93039900, -0.14077200, 0.00546800],
-        [5.03086100, 1.76796000, 1.00339300],
-        [4.53551700, -1.98670100, -1.00709300],
-        [6.27015400, -0.37729000, -0.03216100],
-        [6.73121600, 0.36147000, 0.38477300]
-    ],
-    calculator=model.ase()
-)
+# Define Levodopa molecule with atomic positions
+symbols = 'CCCCCCHHHOHOHCHHCHCOHONHH'
+positions = [
+    [-1.20470200,  1.04669100,  0.26250000],
+    [-0.33574100, -0.02743200,  0.48551100],
+    [-0.84692000, -1.31870700,  0.39783600],
+    [-2.19238900, -1.52237200,  0.09385500],
+    [-3.05023700, -0.45328500, -0.12481600],
+    [-2.54558400,  0.85223500, -0.03894300],
+    [-0.82472500,  2.06685200,  0.32783700],
+    [-0.18721900, -2.16802000,  0.54518100],
+    [-2.58479100, -2.53652600,  0.02508300],
+    [-4.37261400, -0.59290800, -0.42227300],
+    [-4.57886800, -1.53553900, -0.44733400],
+    [-3.42015500,  1.87301800, -0.26082800],
+    [-2.94289200,  2.70701200, -0.16828600],
+    [ 1.11614000,  0.24576200,  0.80827500],
+    [ 1.27565300,  1.32419900,  0.89777300],
+    [ 1.38102000, -0.19348300,  1.78068300],
+    [ 2.10161400, -0.29430300, -0.24020800],
+    [ 1.67791000, -0.09208500, -1.23400000],
+    [ 3.40080900,  0.51505800, -0.19580400],
+    [ 4.49079300, -0.23877900, -0.45353800],
+    [ 5.24402400,  0.37496400, -0.44120200],
+    [ 3.47847700,  1.70324300,  0.00718700],
+    [ 2.26565900, -1.73376100, -0.11776600],
+    [ 2.85858600, -2.08253000, -0.86484600],
+    [ 2.75174500, -1.94700500,  0.74984100]
+]
 
-# Set angle constraint
-angle = [13, 15, 17]
-angle_value = atoms.get_angle(*angle)
-atoms.set_angle(*angle, 126)
-constraint = FixInternals(angles_deg=[[angle_value, angle]])
+# Create ASE Atoms object and attach ANI calculator
+atoms = Atoms(symbols, positions=positions)
+atoms.calc = model.ase()
+
+# Define and set dihedral angle constraint
+dihedral_indices = [2, 1, 13, 16]
+initial_dihedral = atoms.get_dihedral(*dihedral_indices)
+atoms.set_dihedral(*dihedral_indices, 70.0)
+
+# Apply constraint to fix the dihedral angle during optimization
+constraint = FixInternals(dihedrals_deg=[[initial_dihedral, dihedral_indices]])
 atoms.set_constraint(constraint)
 
-# Optimize geometry
+# Run geometry optimization with BFGS
 optimizer = BFGS(atoms)
 optimizer.run(fmax=0.00001)
+
+# Output final energy
+final_energy = atoms.get_potential_energy()
+print(f"Final energy after constrained optimization: {final_energy:.4f} eV")
